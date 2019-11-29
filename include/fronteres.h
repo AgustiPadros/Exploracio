3bd921821338e8f21313f0a326b45569ad7c9f8b
joanvallve <jvallve@iri.upc.edu>
2019-11-29 10:22:06 +0100

    starting

---
+++ include/fronteres.h
@@ -0,0 +1,15 @@
+/*
+ * fronteres.h
+ *
+ *  Created on: Nov 27, 2019
+ *      Author: joanvallve
+ */
+
+#ifndef INCLUDE_FRONTERES_H_
+#define INCLUDE_FRONTERES_H_
+
+
+
+
+
+#endif /* INCLUDE_FRONTERES_H_ */
